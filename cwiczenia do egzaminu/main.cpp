80280a2f6ec75a9fb54459123fb5db1fddbac452
Daniel Rumin <daniel.rumin@uth.pl>
2020-01-23 13:18:37 +0100

    Initial Commit

---
+++ cwiczenia do egzaminu/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cwiczenia do egzaminu
+//
+//  Created by Daniel on 23/01/2020.
+//  Copyright © 2020 Daniel Rumin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
